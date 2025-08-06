66c577b543dbaa0aad45c6b51f2a71f34964a3ea
Cyber8RU7U5 <colinvloef@gmail.com>
2025-08-06 19:14:37 +0200

    Adding more configuration

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+end
+
+# Created by `pipx` on 2025-07-25 15:24:13
+set PATH $PATH /home/user/.local/bin
