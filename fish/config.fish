ac572e36ba3df3c4f9db0a0687eea1c81db49ff4
Cyber8RU7U5 <colinvloef@gmail.com>
2025-08-08 10:27:03 +0200

    Wofi config files added

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
+    source ~/.config/fish/alias.fish
 end
 
 # Created by `pipx` on 2025-07-25 15:24:13
