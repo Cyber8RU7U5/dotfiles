fbcf51eb687056dd5d6e16dfc4583eee3e2ea651
Cyber8RU7U5 <colinvloef@gmail.com>
2025-08-10 16:11:41 +0200

    Adjusted update command to force run via fish

--- fish/alias.fish
+++ fish/alias.fish
@@ -3,4 +3,4 @@ alias agent="eval \$(ssh-agent -c); ssh-add ~/.ssh/github" # Start ssh agent
 alias wmi="curl ifconfig.co" # Get public IP
 alias top="glances" # Proces monitor
 alias calc="galculator" # Calculator
-alias update-puppet="~/.config/puppet/update.fish" # Fetch git updates and push to .config
+alias update-puppet="fish ~/.config/puppet/update.fish" # Fetch git updates and push to .config
