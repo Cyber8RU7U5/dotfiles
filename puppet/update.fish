fa452256185cc04dbfc1a444d05c9e6f800ced98
Cyber8RU7U5 <colinvloef@gmail.com>
2025-08-10 16:14:02 +0200

    Added messages to update procedure

--- puppet/update.fish
+++ puppet/update.fish
@@ -1,6 +1,11 @@
 #!/usr/bin/fish
 
+echo "Cloning repo to temporary folder"
 git clone -b master https://github.com/Cyber8RU7U5/dotfiles.git /tmp/dotfiles
+echo "Removing .git directory"
 rm -rf /tmp/dotfiles/.git
+echo "Copying to ~/.config"
 cp -r /tmp/dotfiles/* ~/.config
+echo "Removing temporary folder"
 rm -rf /tmp/dotfiles
+echo "Done updating! Please relog or reboot"
