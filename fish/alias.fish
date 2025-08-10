ef89fe05526eaee39f20644ed35a1cf37237d528
Cyber8RU7U5 <colinvloef@gmail.com>
2025-08-10 15:49:05 +0200

    Added alias comments and update command

--- fish/alias.fish
+++ fish/alias.fish
@@ -1,5 +1,6 @@
-alias vi="nvim"
-alias agent="eval \$(ssh-agent -c); ssh-add ~/.ssh/github"
-alias wmi="curl ifconfig.co"
-alias top="glances"
-alias calc="galculator"
+alias vi="nvim" # Habit alias
+alias agent="eval \$(ssh-agent -c); ssh-add ~/.ssh/github" # Start ssh agent
+alias wmi="curl ifconfig.co" # Get public IP
+alias top="glances" # Proces monitor
+alias calc="galculator" # Calculator
+alias update-puppet="~/.config/puppet/update.fish" # Fetch git updates and push to .config
